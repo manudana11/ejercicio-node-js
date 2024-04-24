3d2211e6353332b00e41974f29374917145ca7df
manudana11 <manueldanapallardo@gmail.com>
2024-04-24 23:17:23 +0200

    ojo que lo ultimo no sale

---
+++ main.js
@@ -0,0 +1,34 @@
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer((req, res) => {
+//     fs.readFile('pages/home.html', (err, data) => {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(8080);
+
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const filename = "." + q.pathname + ".html";
+   
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          console.error(err);
+          return res.end("404 Not Found");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+  })
+  .listen(8080);
+
+  
